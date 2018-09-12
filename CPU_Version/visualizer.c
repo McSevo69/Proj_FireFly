d69d32a094a14e85bea362389101c9cfac6c8f04
McSevo69 <staudinger.sevi@gmail.com>
2018-09-12 14:54:19 +0200

    lib fix

--- CPU_Version/visualizer.c
+++ CPU_Version/visualizer.c
@@ -1,6 +1,5 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_image.h>
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
