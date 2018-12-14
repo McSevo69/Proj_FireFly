92d96ae7dcfb2e35c651c8a4d7054c785d85ffb6
McSevo69 <staudinger.sevi@gmail.com>
2018-12-14 18:27:02 +0100

    benchmark params

--- Max_Version/CPUCode/VectorsCpuCode.c
+++ Max_Version/CPUCode/VectorsCpuCode.c
@@ -611,7 +611,9 @@ int main(int argc, char *argv[]) {
 		char filename[16];
 		snprintf(filename, sizeof(filename), "benchmark.csv");
 		time_res = fopen(filename, "a");
-		fprintf(time_res, "%d, %d, %zu, %d, %f, %f, %f\n", Vectors_width, Vectors_height, radius, it, timeSpentCPU, timeSpent, timeSpentCPU/timeSpent);
+		//header:
+		//width, height, radius, initial fire cells, it, it until convergence, time CPU, time DFE, Speedup (CPU/DFE)
+		fprintf(time_res, "%d, %d, %zu, %d, %d, %d, %f, %f, %f\n", Vectors_width, Vectors_height, radius, burningOnes, it, x, timeSpentCPU, timeSpent, timeSpentCPU/timeSpent);
 		fclose(time_res);
 		printf("Benchmark results written to %s\n", filename);
 
